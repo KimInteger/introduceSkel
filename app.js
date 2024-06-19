e027e746b89a8351341531ad197e2702f6e74697
KimInteger <kjs20151240@gmail.com>
2024-06-19 22:15:39 +0900

    gpt가 적어준 server

--- app.js
+++ app.js
@@ -1,60 +1,59 @@
 const http = require('node:http');
-
 const fs = require('node:fs');
-
-const path = require("node:path");
+const path = require('node:path');
 
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
   console.log(req.url);
-  if(req.method === 'GET'){
-    if(req.url === '/'){
-      fs.readFile('./public/index.html',(err,data)=>{
-        if(err){
-          res.writeHead(500,{"Content-Type":"text/plain; charset=UTF-8"});
-          res.end('서버 자체 에러');
-        } else {
-          res.writeHead(200,{"Content-Type":"text/html; charset=UTF-8"});
-          res.end(data);
-        }
-      });
-    } else if (req.url === '/hrim') {
-      res.writeHead(200,{"Content-Type":"text/html; charset=UTF-8"});
-      res.end(`나는 코딩 뚝딱이들의 리더 헤이호다!`);
-    } else if (req.url === '/index.html') {
-      fs.readFile('./public/index.html',(err,data)=>{
-        if(err){
-          res.writeHead(500,{"Content-Type":"text/plain; charset=UTF-8"});
-          res.end('서버 자체 에러');
-        } else {
-          res.writeHead(200,{"Content-Type":"text/html; charset=UTF-8"});
-          res.end(data);
-        }
-      });
-    } else if (req.url === '/script.js') {
-      fs.readFile('./public/script.js',(err,data)=>{
-        if(err){
-          res.writeHead(500,{"Content-Type":"text/plain; charset=UTF-8"});
-          res.end('서버 자체 에러');
-        } else {
-          res.writeHead(200,{"Content-Type":"application/javascript; charset=UTF-8"});
-          res.end(data);
-        }
-      });
-    } else {
-      res.writeHead(404,{"Content-Type":"text/plain charset=UTF-8"});
-      res.end('페이지를 찾을 수 없습니다.');
+  if (req.method === 'GET') {
+    let filePath = './public' + (req.url === '/' ? '/index.html' : req.url);
+    let extname = path.extname(filePath);
+    let contentType = 'text/html; charset=UTF-8';
+
+    switch (extname) {
+      case '.js':
+        contentType = 'application/javascript; charset=UTF-8';
+        break;
+      case '.css':
+        contentType = 'text/css; charset=UTF-8';
+        break;
+      case '.png':
+        contentType = 'image/png';
+        break;
+      case '.jpg':
+        contentType = 'image/jpg';
+        break;
+      case '.gif':
+        contentType = 'image/gif';
+        break;
     }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        if (err.code == 'ENOENT') {
+          fs.readFile('./public/404.html', (err, data) => {
+            res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' });
+            res.end(data, 'utf-8');
+          });
+        } else {
+          res.writeHead(500, { 'Content-Type': 'text/plain; charset=UTF-8' });
+          res.end('서버 오류: ' + err.code);
+        }
+      } else {
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data, 'utf-8');
+      }
+    });
   } else {
-    res.writeHead(404,{"Content-Type":"text/plain charset=UTF-8"});
-    res.end('페이지를 찾을 수 없습니다.');
+    res.writeHead(405, { 'Content-Type': 'text/plain; charset=UTF-8' });
+    res.end('메서드가 허용되지 않음');
   }
 });
 
-server.listen(PORT,(err)=>{
-  if(err){
-    console.error('에러발생' + err);
+server.listen(PORT, err => {
+  if (err) {
+    console.error('에러발생: ' + err);
   } else {
     console.log('서버 오픈');
     console.log(`http://localhost:${PORT}`);
