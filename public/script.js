6e7232918777fdb032e081d48498f8661bbaf2ca
KimInteger <kjs20151240@gmail.com>
2024-06-19 21:45:32 +0900

    xhr요청 기능 추가

--- public/script.js
+++ public/script.js
@@ -7,3 +7,18 @@ const ehyun = document.getElementById('yoshi');
 const hoyeon = document.getElementById('pinokio');
 
 const yeonsung = document.getElementById('likeBlack');
+
+const xhr = new XMLHttpRequest();
+
+hrim.addEventListener('click',()=>{
+  xhr.open('GET','/hrim');
+  xhr.send();
+
+  xhr.addEventListener('load',()=>{
+    if(xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      throw new Error('에러 발생!');
+    }
+  });
+});
