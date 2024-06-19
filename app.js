7dde2a2d1627bfd425f63ef7ffd348171917402a
KimInteger <kjs20151240@gmail.com>
2024-06-19 21:50:04 +0900

    server초기화 및 포트에 연결

--- app.js
+++ app.js
@@ -10,7 +10,26 @@ const server = http.createServer((req,res)=>{
   console.log(req.url);
   if(req.method === 'GET'){
     if(req.url === '/'){
-      
+
+    } else if (req.url === '/hrim') {
+
+    } else if (req.url === '/index.html') {
+
+    } else if (req.url === '/script.js') {
+
+    } else {
+
     }
+  } else {
+
+  }
+});
+
+server.listen(PORT,(err)=>{
+  if(err){
+    console.error('에러발생' + err);
+  } else {
+    console.log('서버 오픈');
+    console.log(`http://localhost:${PORT}`);
   }
 });
