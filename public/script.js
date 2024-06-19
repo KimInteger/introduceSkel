e25025c384d78637aa5591473baf15dbc95fa495
KimInteger <kjs20151240@gmail.com>
2024-06-19 21:43:10 +0900

    각 id값을 찾아 각 멤버를 식별 및 변수에 할당

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
-console.log('script.js');
+const hrim = document.getElementById('leader');
+
+const js = document.getElementById('waruru');
+
+const ehyun = document.getElementById('yoshi');
+
+const hoyeon = document.getElementById('pinokio');
+
+const yeonsung = document.getElementById('likeBlack');
